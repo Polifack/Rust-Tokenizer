8cd5704f3efc9de017e13e45c922dfd0a4c24cf0
Poli <diegoroca.ordes@gmail.com>
2022-11-10 15:53:18 +0100

    added demo

--- target/debug/deps/sentence_parser-344069400588dcac.d
+++ target/debug/deps/sentence_parser-344069400588dcac.d
@@ -1,5 +1,5 @@
-/home/poli/Projects/Rust/sentence_parser/target/debug/deps/sentence_parser-344069400588dcac: src/main.rs
+/home/poli/Projects/Rust-Tokenizer/target/debug/deps/sentence_parser-344069400588dcac: src/main.rs
 
-/home/poli/Projects/Rust/sentence_parser/target/debug/deps/sentence_parser-344069400588dcac.d: src/main.rs
+/home/poli/Projects/Rust-Tokenizer/target/debug/deps/sentence_parser-344069400588dcac.d: src/main.rs
 
 src/main.rs:
--- target/debug/sentence_parser.d
+++ target/debug/sentence_parser.d
@@ -1,1 +1,1 @@
-/home/poli/Projects/Rust/sentence_parser/target/debug/sentence_parser: /home/poli/Projects/Rust/sentence_parser/src/main.rs
+/home/poli/Projects/Rust-Tokenizer/target/debug/sentence_parser: /home/poli/Projects/Rust-Tokenizer/src/main.rs
